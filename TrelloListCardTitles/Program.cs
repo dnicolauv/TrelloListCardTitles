0a6d828f50dbbcb7cf4df14a2402497316b7aeac
David Nicolau <ulin666@gmail.com>
2018-12-12 16:25:50 +0100

    Initial commit

--- TrelloListCardTitles/Program.cs
+++ TrelloListCardTitles/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Manatee.Trello;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,33 @@ namespace TrelloListCardTitles
     {
         static void Main(string[] args)
         {
+            Task task = ReadData();
+            task.Wait();
+        }
+
+        private static async Task ReadData()
+        {
+            try
+            {
+                TrelloAuthorization.Default.AppKey = "";
+                TrelloAuthorization.Default.UserToken = "";
+
+                ITrelloFactory factory = new TrelloFactory();
+                var board = factory.Board("nC8QJJoZ");
+                
+                await board.Refresh();
+                var cards = board.Cards;
+                await cards.Refresh();
+
+                foreach (Card card in cards)
+                    Console.WriteLine(card.Name);
+
+                Console.ReadLine();
+
+            }catch(Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
